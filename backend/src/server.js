f4b4ca3ce3954c6de84068fabf37b0b3f31a60bb
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-08-06 11:54:18 -0300

    criação do modelo Dev, do controller DevController e utilização do mongoDb para armazenar os usuários cadastrados na aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
+const mongooseConnect = require('./MongooseConnect');
 
 const server = express();
+
+//comentar esta linha antes de commitar e fazer o push por causa da senha ↓↓↓↓
+mongoose.connect(mongooseConnect, {
+  useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
