132aec83ecd7ce6e2730243940e0bad88ad1b1b8
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-08-06 13:31:46 -0300

    Instalação do cors e backend finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const mongooseConnect = require('./MongooseConnect');
+const cors = require('cors');
 
 const server = express();
 
@@ -9,6 +10,7 @@ mongoose.connect(mongooseConnect, {
   useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
