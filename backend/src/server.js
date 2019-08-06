6a7d3bd02a6f91822d0212fbd76848ff12e5dd51
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-08-06 13:08:46 -0300

    implementação dos controllers de likes e dislikes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,6 @@ const mongooseConnect = require('./MongooseConnect');
 
 const server = express();
 
-//comentar esta linha antes de commitar e fazer o push por causa da senha ↓↓↓↓
 mongoose.connect(mongooseConnect, {
   useNewUrlParser: true
 });
