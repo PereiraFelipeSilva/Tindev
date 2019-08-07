5512dbe46d73f0a808fa0ca9b2ec1fbb712eb2fc
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-08-07 17:02:46 -0300

    Utilização do useEffect pra listar os usuários na rota /devs

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
+import api from '../services/api';
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
 
 export default function Main({ match }){
 
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers(){
+
+      const response = await api.get('/devs', {
+        headers: {
+          user: match.params.id
+        }
+      });
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
   return(
 
     <div className="main-container">
       <img src={logo} alt="Tindev-logo" />
       <ul>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
+        {users.map(user => (
+
+        <li key={user._id}>
+          <img src={user.avatar} alt={user.name} />
           <footer>
-            <strong>Diego Fernandes</strong>
-            <p>CTO na RocketSeat</p>
+            <strong>{user.name}</strong>
+            <p>{user.bio}</p>
           </footer>
 
           <div className="buttons">
@@ -27,54 +46,7 @@ export default function Main({ match }){
             </button>
           </div>
         </li>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
-          <footer>
-            <strong>Diego Fernandes</strong>
-            <p>CTO na RocketSeat</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike-button" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like-button" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
-          <footer>
-            <strong>Diego Fernandes</strong>
-            <p>CTO na RocketSeat</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike-button" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like-button" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img src="https://avatars2.githubusercontent.com/u/2254731?v=4" alt="" />
-          <footer>
-            <strong>Diego Fernandes</strong>
-            <p>CTO na RocketSeat</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="dislike-button" />
-            </button>
-            <button type="button">
-              <img src={like} alt="like-button" />
-            </button>
-          </div>
-        </li>
+        ))}
       </ul>
     </div>
   );
