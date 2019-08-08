6360756c5ddad5d6dc5ae9844c2583a0bc2393b4
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-08-08 15:03:36 -0300

    Criação e estilização da página inicial (login)

--- tindev/src/index.js
+++ tindev/src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import Login from './pages/Login';
 
 export default function App(){
 
   return (
 
-    <View style={styles.container}>
-      <Text style={styles.text}>Uatafoque Men</Text>
-    </View>
+    <Login />
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#7159c1',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    text: {
-      fontWeight: 'bold',
-      fontSize: 20,
-      color: '#FFF'
-    }
-});
---
+++ tindev/src/pages/Login.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Image, TextInput, Text, TouchableOpacity } from 'react-native';
+import logo from '../assets/logo.png';
+
+export default function Login(){
+
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      enabled={Platform.OS === 'ios'}
+      style={styles.container}
+    >
+      <Image source={logo} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário do Github"
+        placeholderTextColor="#999"
+        style={styles.input}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30
+  },
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#ddd',
+    marginTop: 20,
+    paddingHorizontal: 15
+  },
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+});
