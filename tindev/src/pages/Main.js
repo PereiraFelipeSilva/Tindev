b842c6186b641bd38d38b182a2a16f9504f423e6
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-08-08 18:47:45 -0300

    Utilização do axios pra buscar os dados dos usuários na api do github

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, Image, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import api from '../services/api';
 import logo from '../assets/logo.png';
 import dislike from '../assets/dislike.png';
 import like from '../assets/like.png';
 
-export default function Main(){
+export default function Main({ navigation }){
+
+  const id = navigation.getParam('user');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers(){
+
+      const response = await api.get('/devs', {
+        headers: {
+          user: id
+        }
+      });
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [id]);
+
+  async function handleLike(id){
+
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: { user: id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDislike(id){
+
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
 
   return (
 
@@ -12,27 +48,21 @@ export default function Main(){
       <Image style={styles.logo} source={logo}/>
 
       <View style={styles.cardsContainer}>
-        <View style={[styles.card, {zIndex: 3}]}>
-          <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/44786331?v=4' }}/>
-          <View style={styles.footer}>
-            <Text style={styles.name}>Felipe Pereira</Text>
-            <Text style={styles.bio} numberOfLines={3}>Estudante de desenvolvimento frontend</Text>
+        { users.length === 0 
+          ? <Text style={styles.emptyList}>
+            Não existem devs compatíveis no momento :(
+            </Text>
+          : (
+          users.map((user, index) => (
+            <View key={user._id} style={[styles.card, {zIndex: users.length - index}]}>
+            <Image style={styles.avatar} source={{ uri: user.avatar }}/>
+            <View style={styles.footer}>
+              <Text style={styles.name}>{user.name}</Text>
+              <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
+            </View>
           </View>
-        </View>
-        <View style={[styles.card, {zIndex: 2}]}>
-          <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/44786331?v=4' }}/>
-          <View style={styles.footer}>
-            <Text style={styles.name}>Felipe Pereira</Text>
-            <Text style={styles.bio} numberOfLines={3}>Estudante de desenvolvimento frontend</Text>
-          </View>
-        </View>
-        <View style={[styles.card, {zIndex: 1}]}>
-          <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/44786331?v=4' }}/>
-          <View style={styles.footer}>
-            <Text style={styles.name}>Felipe Pereira</Text>
-            <Text style={styles.bio} numberOfLines={3}>Estudante de desenvolvimento frontend</Text>
-          </View>
-        </View>
+          ))
+        )}
       </View>
 
       <View style={styles.buttonsContainer}>
@@ -115,5 +145,12 @@ const styles = StyleSheet.create({
       width: 0,
       height: 2
     }
+  },
+  emptyList: {
+    alignSelf: 'center',
+    color: '#999',
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 10
   }
 });
